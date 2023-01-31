e4f48dc76460b08a6d2e96be67bdbbedc88c3209
hojun313 <hojun313@naver.com>
2023-01-31 13:35:58 +0900

    캔버스 생성, 이미지 로드, 캔버스 로드

--- main.js
+++ main.js
@@ -5,3 +5,33 @@ ctx = canvas.getContext('2d');
 canvas.width = 800;
 canvas.height = 600;
 document.body.appendChild(canvas);
+
+let backgroundImage,spaceshipImage,enemyImage,bulletImage,gameOverImage;
+let spaceshipX = canvas.width/2 - 32;
+let spaceshipY = canvas.height - 64;
+
+function loadImage(){
+    backgroundImage = new Image();
+    backgroundImage.src = 'images/background.jpg';
+    spaceshipImage = new Image();
+    spaceshipImage.src = 'images/ship.png';
+    enemyImage = new Image();
+    enemyImage.src = 'images/enemy.png';
+    bulletImage = new Image();
+    bulletImage.src = 'images/bullet.png';
+    gameOverImage = new Image();
+    gameOverImage.src = 'images/gameover.png';
+}
+
+function render(){
+    ctx.drawImage(backgroundImage,0,0,canvas.width,canvas.height);
+    ctx.drawImage(spaceshipImage,spaceshipX,spaceshipY);
+}
+
+function main(){
+    render();
+    requestAnimationFrame(main);
+}
+
+loadImage();
+main();
